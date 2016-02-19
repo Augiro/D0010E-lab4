04176fe7be58e75cad03b8eb3c5e599f496f8b61
August <august.eriksson@outlook.com>
2016-02-19 15:02:09 +0100

    Fixed main method if no argument is present

--- src/lab4/GomokuMain.java
+++ src/lab4/GomokuMain.java
@@ -7,15 +7,17 @@ import lab4.client.GomokuClient;
  public class GomokuMain {
      public static void main(String[] args){
          if(args.length==0){
-             GomokuClient testclient=new GomokuClient(4008);
+             GomokuClient client=new GomokuClient(4008);
+             GomokuGameState gamestate = new GomokuGameState(client);
+             GomokuGUI gui = new GomokuGUI(gamestate, client);
          } else{
              int port = Integer.parseInt(args[0]);
              GomokuClient testclient=new GomokuClient(port);
              GomokuClient testclient2=new GomokuClient(port-1);
              GomokuGameState gamestate=new GomokuGameState(testclient);
              GomokuGameState gamestate2=new GomokuGameState(testclient2);
-             GomokuGUI testGUI=new GomokuGUI(gamestate,testclient);
-             GomokuGUI testGUI2=new GomokuGUI(gamestate2,testclient2);
+             GomokuGUI gu1=new GomokuGUI(gamestate,testclient);
+             GomokuGUI gu2=new GomokuGUI(gamestate2,testclient2);
          }
      }
  }
