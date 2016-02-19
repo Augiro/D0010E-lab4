952b2b92b72ec2888cd22ee6d06ee7062e1494a3
August <august.eriksson@outlook.com>
2016-02-19 13:52:18 +0100

    Added GomokuMain class

--- src/lab4/data/GomokuGameState.java
+++ src/lab4/data/GomokuGameState.java
@@ -15,7 +15,7 @@ import lab4.client.GomokuClient;
 public class GomokuGameState extends Observable implements Observer{
 
    // Game variables
-	private final int DEFAULT_SIZE = 15;
+	private final int DEFAULT_SIZE = 5;
 	private GameGrid gameGrid;
 	
     //Possible game states
