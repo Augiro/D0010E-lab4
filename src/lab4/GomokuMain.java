8348a1befbcebf155f23342de6a5658fd42e84c2
August <august.eriksson@outlook.com>
2016-02-21 15:04:42 +0100

    Simplified main method

--- src/lab4/GomokuMain.java
+++ src/lab4/GomokuMain.java
@@ -6,18 +6,9 @@ import lab4.client.GomokuClient;
 
  public class GomokuMain {
      public static void main(String[] args){
-         if(args.length==0){
-             GomokuClient client=new GomokuClient(4008);
-             GomokuGameState gamestate = new GomokuGameState(client);
-             GomokuGUI gui = new GomokuGUI(gamestate, client);
-             GomokuClient client2=new GomokuClient(4007);
-             GomokuGameState gamestate2 = new GomokuGameState(client2);
-             GomokuGUI gui2 = new GomokuGUI(gamestate2, client2);
-         } else{
-             int port = Integer.parseInt(args[0]);
-             GomokuClient client=new GomokuClient(port);
-             GomokuGameState gamestate=new GomokuGameState(client);
-             GomokuGUI gu1=new GomokuGUI(gamestate,client);
-         }
+         int port = args.length == 1 ? Integer.parseInt(args[0]) : 4008;
+         GomokuClient client=new GomokuClient(port);
+         GomokuGameState gamestate = new GomokuGameState(client);
+         GomokuGUI gui = new GomokuGUI(gamestate, client);
      }
  }
